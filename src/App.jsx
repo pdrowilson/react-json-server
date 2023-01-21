9d00cadc1b2ad5f7fae54dbb6321f993d3afaa42
Pedro Wilson <pdro.wilson@gmail.com>
2023-01-21 07:15:13 -0300

    chore: add tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { useState, useEffect } from 'react'
 
 function App() {
@@ -21,16 +20,18 @@ function App() {
   console.log(animes);
   
   return (
-    <>
-      <h1>Animes</h1>
-      <ul>
-        {animes.map((anime, index) => 
-          <li key={index}>
-            <span>{anime.title}</span>: {anime.episodes} episodes | {anime.year}
-          </li>
-        )}
-      </ul>
-    </>
+    <div className='flex justify-center h-screen'>
+      <div className='self-center'>
+        <h1 className='text-2xl text-blue-600'>Animes</h1>
+        <ul>
+          {animes.map((anime, index) => 
+            <li key={index}>
+              <span>{anime.title}</span>: {anime.episodes} episodes | {anime.year}
+            </li>
+          )}
+        </ul>
+      </div>
+    </div>
   )
 }
 
