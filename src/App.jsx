1d4ff44a1c97515e7b3546e50e1474703b008ac8
Pedro Wilson <pdro.wilson@gmail.com>
2023-01-21 06:05:57 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [animes, setAnimes] = useState([])
+  const url = 'http://localhost:3000/animes'
+
+  useEffect(() => {
+    async function fetchData() {
+
+      const res = await fetch(url)
+      
+      const data = await res.json()
+      
+      setAnimes(data)
+    }
+    fetchData()
+
+  }, [])
+
+  console.log(animes);
+  
+  return (
+    <>
+      <h1>Animes</h1>
+      <ul>
+        {animes.map((anime) => 
+          <li key={anime.id}>
+            <span>{anime.title}</span>: {anime.episodes} episodes | {anime.year}
+          </li>
+        )}
+      </ul>
+    </>
+  )
+}
+
+export default App
